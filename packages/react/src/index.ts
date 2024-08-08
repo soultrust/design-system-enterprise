57d00bf5b7374250a67101797de15496bb1dce44
Tomo Suzuki <tomo@soultrust.com>
2024-08-07 21:52:39 -0400

    Start of playgrounds

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,6 +1,7 @@
+import Button from './atoms/Button';
 import { Color } from "./atoms/Color";
 import { Margin } from "./atoms/Margin";
 import { Text } from "./atoms/Text";
 import { Select } from "./molecules/Select";
 
-export { Color, Margin, Text, Select};
+export { Button, Color, Margin, Text, Select };
